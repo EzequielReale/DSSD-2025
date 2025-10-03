6b09e7d7605b11b46a3a72f913e18cbb4535c8c4
Emilia Romero <m.emiliaromero4@gmail.com>
2025-10-02 23:41:59 -0300

    Migro de Sqlite a PostgreSQL

--- ProjectPlanning/settings.py
+++ ProjectPlanning/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'ProjectPlanning.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.getenv("DB_NAME", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.getenv("DB_USER", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", ""),
     }
 }
 
